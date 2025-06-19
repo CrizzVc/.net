fd0f0e707edc34344f2e6ccb81471b6134cfd741
Criz <crizvc3@gmail.com>
2025-06-19 17:12:15 -0500

    limpieza

--- repos/ConsoleApp1/ConsoleApp1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.VisualBasic;
-
-Console.WriteLine("Hello, World!");
-Console.WriteLine("este es un textoo!!");
-
--- repos/resitencias/resitencias/Module1.vb
+++
@@ -1,3 +0,0 @@
-﻿Module Module1
-    Public color1, color2, color3, color4, color5, color6 As String
-End Module
